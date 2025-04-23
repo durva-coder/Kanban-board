3469606efb750a1322830fab9a059d2091e5136d
durvabanthia <durvarbanthia@gmail.com>
2025-04-23 23:11:33 +0530

    some changes.

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ const taskRoutes = require("./api/routes/taskRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection
